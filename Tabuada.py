632e911bd5cfd6cafa49bf152652f46f8b61f93f
Jeff <114240078+Jeffin24@users.noreply.github.com>
2024-08-20 20:07:57 -0300

    Add files via upload

---
+++ Tabuada.py
@@ -0,0 +1,10 @@
+print("TABUADA") 
+print("--------")
+v=int(input("Qual tabuada deseja imprimir? "))
+for valor in range(1,11,1):
+        print (v ,"x",valor,"=",v*valor)
+        
+
+
+
+    
---
+++ fatorial.py
@@ -0,0 +1,7 @@
+f=int(input("Digite o numéro que você quer o fatoria:"))
+result=1
+for valor in range(f,1,-1):
+    result*=valor
+   
+   
+print(f,"!","=",result)  
